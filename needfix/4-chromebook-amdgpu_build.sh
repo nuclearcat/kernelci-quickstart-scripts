4bde545e42a8f15b08fcabcc79578d7c328299d8
Denys Fedoryshchenko <denys.f@collabora.com>
2022-09-06 09:57:07 +0300

    Update examples, as docker images names changed and new feature being added to fetch firmware

---
+++ needfix/4-chromebook-amdgpu_build.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+cwd=$(pwd)
+echo Kill old and setup new build docker
+sudo docker stop kernelci-build
+sudo docker rm kernelci-build
+sudo docker run -it -v ${cwd}:/root/kernelci-quickstart --name kernelci-build kernelci/build-gcc-10_x86 /root/kernelci-quickstart/scripts/build/build_chromebook_fragment.sh
+
+echo docker rootfs
+sudo docker stop kernelci-debos
+sudo docker rm kernelci-debos
+sudo docker run -itd -v ${cwd}:/root/kernelci-quickstart --name kernelci-debos --device /dev/kvm --privileged kernelci/debos
+sudo docker exec -it kernelci-debos /root/kernelci-quickstart/scripts/rootfs/rootfs.sh
+
--- scripts/build/build_chromebook_fragment.sh
+++ scripts/build/build_chromebook_fragment.sh
@@ -1,15 +1,18 @@
 #!/bin/sh
 PARAM="--settings=kernelci-x86_64-clang13.conf"
-cd /root/kernelci-quickstart/kernelci-core
+ls -la
+#git clone --depth=1 https://git.kernel.org/pub/scm/linux/kernel/git/firmware/linux-firmware.git
+cd kernelci-quickstart/kernelci-core
 echo Update repo
-./kci_build ${PARAM} update_repo --build-config=stable_5.10 --mirror=linux-mirror.git
+./kci_build ${PARAM} update_repo --build-config=stable_5.15 --mirror=linux-mirror.git
 echo generate_fragments
-./kci_build ${PARAM} generate_fragments --build-config=stable_5.10
+./kci_build ${PARAM} generate_fragments --build-config=stable_5.15
 echo Init BMeta
-./kci_build ${PARAM} init_bmeta --build-config=stable_5.10
+./kci_build ${PARAM} init_bmeta --build-config=stable_5.15
 echo make_config
-./kci_build ${PARAM} make_config --defconfig=cros://chromeos-5.10/x86_64/chromeos-intel-pineview.flavour.config+x86-chromebook
-exit
+./kci_build ${PARAM} make_config --defconfig=cros://chromeos-5.15/x86_64/chromeos-amd-stoneyridge.flavour.config+x86-chromebook+CONFIG_MODULE_COMPRESS_GZIP=n
+echo fetch_firmware
+./kci_build ${PARAM} fetch_firmware
 echo make_kernel
 ./kci_build ${PARAM} make_kernel
 echo make_modules
