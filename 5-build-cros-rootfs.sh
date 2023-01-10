2cbd83c5f254fe673748a0901dfb85ec84437148
Denys Fedoryshchenko <denys.f@collabora.com>
2023-01-10 13:59:34 +0200

    Simplify a little bit cros-rootfs

--- 5-build-cros-rootfs.sh
+++ 5-build-cros-rootfs.sh
@@ -1,17 +1,15 @@
 #!/bin/sh
-#docker stop kernelci-build-cros
-#docker rm kernelci-build-cros
-#cd kernelci-core
-#docker run \
-#    --name kernelci-build-cros \
-#    -itd \
-#    -v $(pwd):/kernelci-core \
-#    --device /dev/kvm \
-#    -v /dev:/dev \
-#    --privileged \
-#    --user cros \
-#    kernelci/cros-sdk:kernelci
-#exit
+docker stop kernelci-build-cros
+docker rm kernelci-build-cros
+docker run \
+    --name kernelci-build-cros \
+    -itd \
+    -v $(pwd)/kernelci-core:/kernelci-core \
+    --device /dev/kvm \
+    -v /dev:/dev \
+    --privileged \
+    --user cros \
+    kernelci/cros-sdk
 
 docker exec \
     -it kernelci-build-cros \
@@ -22,7 +20,7 @@ docker exec \
     bash -c 'cd /kernelci-core;
     ./kci_rootfs \
     build \
-    --rootfs-config chromiumos-kukui \
+    --rootfs-config chromiumos-trogdor \
     --data-path /kernelci-core/config/rootfs/chromiumos \
     --arch arm64 \
     --output /kernelci-core/temp
