1a3efbaa9ae3b8b15fc4a3e236c00873273bfd95
Denys Fedoryshchenko <denys.f@collabora.com>
2023-03-28 11:00:23 +0300

    Trying to improve and shorted time of scripts generation

--- 1-prepare-environment.sh
+++ 1-prepare-environment.sh
@@ -8,6 +8,31 @@ echo Kill old and setup new build docker
 sudo docker stop kernelci-build
 sudo docker rm kernelci-build
 
-sudo docker run -it -v ${cwd}:/home/kernelci/kernelci-quickstart --name kernelci-build kernelci/kernelci /home/kernelci/kernelci-quickstart/scripts/prepare/mirror.sh
+# if kernelci-core exist - stop
+if [ -d "kernelci-core" ]; then
+    echo "kernelci-core exist"
+    exit 1
+fi
+
+
+# if argument set to kernel
+if [ "$1" = "kernel" ]; then
+    echo "Building kernel"
+    UID=1000
+else
+    echo "Building rootfs"
+    UID=996
+fi
+cd ../
+chown -R ${UID} kernelci-quickstart-scripts
+cd -
+#git clone -b staging.kernelci.org https://github.com/kernelci/kernelci-core.git
+git clone -b chromeos https://github.com/kernelci/kernelci-core.git
+#git clone -b dev/aratiu/bump-kernel-add-cherry-support https://github.com/10ne1/kernelci-core
+cp cfg/* kernelci-core/
+chown -R ${UID} kernelci-core
+if [ "$1" = "kernel" ]; then
+    sudo docker run -it -v ${cwd}:/home/kernelci/kernelci-quickstart --name kernelci-build kernelci/kernelci /home/kernelci/kernelci-quickstart/scripts/prepare/mirror.sh
+fi
 mkdir -p kernelci-core/temp
-chown -R 996 kernelci-core
+chown -R ${UID} kernelci-core/temp
--- scripts/prepare/mirror.sh
+++ scripts/prepare/mirror.sh
@@ -1,10 +1,5 @@
 #!/bin/sh
-cd /home/kernelci/kernelci-quickstart
+id
+cd /home/kernelci/kernelci-quickstart/kernelci-core
 # you can use also some user repository, to check someone patches
-#git clone -b staging.kernelci.org https://github.com/kernelci/kernelci-core.git
-git clone -b dev/aratiu/bump-kernel-add-cherry-support https://github.com/10ne1/kernelci-core
-cp cfg/* kernelci-core/
-cd kernelci-core
-# optionally use branch
 kci_build update_mirror --build-config=next --mirror=linux-mirror.git
-mkdir temp
